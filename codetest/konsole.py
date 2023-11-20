30d55784923f41fc66aa47c3895e91a67173f38d
amusukwa <amusukwa@gmail.com>
2023-11-20 01:18:15 -0800

    task 2 test

--- codetest/konsole.py
+++ codetest/konsole.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] == '{' and pline[-1] =='}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -119,37 +119,31 @@ class HBNBCommand(cmd.Cmd):
             class_name = args.split(" ")[0]
         except IndexError:
             pass
-
         if not class_name:
             print("** class name missing **")
             return
-
-        
-
         elif class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
+        # create Place city_id="0001" user_id="0001" name="My_little_house"
         all_list = args.split(" ")
+
         new_instance = eval(class_name)()
 
         for i in range(1, len(all_list)):
             key, value = tuple(all_list[i].split("="))
-
             if value.startswith('"'):
                 value = value.strip('"').replace("_", " ")
-            else:                
+            else:
                 try:
                     value = eval(value)
-                    
                 except Exception:
                     print(f"** couldnt evaluate {value}")
                     pass
             if hasattr(new_instance, key):
                 setattr(new_instance, key, value)
-    
-
-    
-        storage.save()
+        
+        storage.new(new_instance)
         print(new_instance.id)
         new_instance.save()
 
