4c39851179304cc3bf7190e754c9e86c621bdbaa
amusukwa <amusukwa@gmail.com>
2023-12-09 04:37:57 -0800

    2-do_deploy_web_static.py 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,36 +1,36 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Fabric script that distributes an archive to web servers using do_deploy
+Fabric script distributes an archive to your web servers, using do_deploy
 """
 
 from fabric.api import *
-from os.path import exists
+from os import path
 
-env.hosts = ['<54.173.33.118>', '<34.207.62.11>']
-env.user = '<ubuntu>'
-env.key_filename = '</root/.ssh/id_rsa>'
+env.hosts = ['<IP web-01>', '<IP web-02>']
+
 
 def do_deploy(archive_path):
     """
-    Distribute an archive to web servers
+    Distributes an archive to your web servers
     """
-    if not exists(archive_path):
+    if not path.exists(archive_path):
         return False
 
     try:
-        # Upload the archive to /tmp/ directory on the web server
-        put(archive_path, '/tmp/')
-
-        # Extract the archive to /data/web_static/releases/
         archive_name = archive_path.split('/')[-1]
         archive_no_ext = archive_name.split('.')[0]
-        run('mkdir -p /data/web_static/releases/{}/'.format(archive_no_ext))
+
+        # Upload archive to /tmp/ directory on the web server
+        put(archive_path, '/tmp/')
+
+        # Uncompress archive
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_no_ext))
         run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_name, archive_no_ext))
 
-        # Remove the archive from the web server
+        # Delete the archive from the web server
         run('rm /tmp/{}'.format(archive_name))
 
-        # Delete the symbolic link /data/web_static/current
+        # Delete the symbolic link /data/web_static/current on the server
         run('rm -rf /data/web_static/current')
 
         # Create a new symbolic link /data/web_static/current
@@ -41,11 +41,6 @@ def do_deploy(archive_path):
         return True
 
     except Exception as e:
-        print(e)
+        print(f"Error: {e}")
         return False
-
-if __name__ == "__main__":
-    # Example usage:
-    archive_path = '/path/to/your/archive.tar.gz'
-    do_deploy(archive_path)
 
