6cbf20ed3ba5cd78ea935cffc4de81d32e18edec
amusukwa <amusukwa@gmail.com>
2024-01-01 14:44:23 -0800

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,6 +7,7 @@ Routes:
 """
 from models import storage
 from flask import Flask
+from models.state import State
 from flask import render_template
 
 app = Flask(__name__)
@@ -18,7 +19,7 @@ def cities_by_states():
 
     States/cities are sorted by name.
     """
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template("8-cities_by_states.html", states=states)
 
 
